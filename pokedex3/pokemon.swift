6baa9956b9810f55579358c2e12f7f8bd7d5a565
mik3ycodez <mjharris0114@gmail.com>
2017-05-27 11:33:54 -0700

    created pokemon class

---
+++ pokedex3/pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  pokemon.swift
+//  pokedex3
+//
+//  Created by cbeuser on 5/27/17.
+//  Copyright © 2017 CBE User. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
