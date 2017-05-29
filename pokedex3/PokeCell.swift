14e4e224eba138c49be0f029f6cdda042cd205e9
mik3ycodez <mjharris0114@gmail.com>
2017-05-28 20:03:12 -0700

    created pokemon array and connected pokemon.csv

--- pokedex3/PokeCell.swift
+++ pokedex3/PokeCell.swift
@@ -23,7 +23,7 @@ class PokeCell: UICollectionViewCell {
         
     }
     
-    func configureCell(pokemon: Pokemon) {
+    func configureCell(_ pokemon: Pokemon) {
         
         self.pokemon = pokemon
         
