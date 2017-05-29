10251335c41da4d1df9c5631a25145216cd35ec0
mik3ycodez <mjharris0114@gmail.com>
2017-05-28 19:04:53 -0700

    added custom poke cell class

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by cbeuser on 5/27/17.
+//  Copyright © 2017 CBE User. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
