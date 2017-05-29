e7c415fc81dbf3b35ff617fe7c0a6e06c1da4abc
mik3ycodez <mjharris0114@gmail.com>
2017-05-28 20:45:19 -0700

    added custom fonts and music player

--- pokedex3/ViewController.swift
+++ pokedex3/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     
     @IBOutlet weak var collection: UICollectionView!
+    @IBOutlet weak var sliderBar: UISlider!
     
+    var musicPlayer: AVAudioPlayer!
     var pokemon = [Pokemon]()
     
     override func viewDidLoad() {
@@ -22,8 +25,48 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collection.delegate = self
         
         parsePokemonCSV()
+        initAudio()
     }
     
+    func initAudio() {
+        
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+        
+        do {
+            
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+    }
+    
+    @IBAction func musicBtnPressed(_ sender: UIButton) {
+        if musicPlayer.isPlaying {
+            
+            musicPlayer.pause()
+            sliderBar.isHidden = true
+            sender.alpha = 0.2
+            
+        } else {
+            
+            musicPlayer.play()
+            sliderBar.isHidden = false
+            sender.alpha = 1.0
+            
+        }
+    }
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        
+        let volumeVal = Float(sender.value)
+        musicPlayer.volume = volumeVal
+        
+    }
     
     func parsePokemonCSV() {
         let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
